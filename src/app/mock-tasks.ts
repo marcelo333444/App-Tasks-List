77454a3595fcd0366e2848c06182c4d2716ba0ef
marcelo333444 <marcelofeliziani8j5@gmail.com>
2022-09-09 02:18:18 -0300

    agregar mock de tareas y el interface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,26 @@
+export const TASKS = [
+       {
+           id: 1,
+           text: "Terminar primer modulo angular",
+           day: "Antes del 20 de sept 2022 a las 19:30",
+           reminder: true,
+       },
+       {
+        id: 2,
+        text: "Investigar sobre Boostrap",
+        day: "Antes del 25 de sept 2022 a las 16:30",
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "repasar conceptos de html, css y js",
+        day: "Antes del 20 de sept 2022 a las 15:30",
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "continuar el royecto con sass y gulp",
+        day: "Antes del 20 de sept 2022 a las 14:30",
+        reminder: true,
+    },
+]
